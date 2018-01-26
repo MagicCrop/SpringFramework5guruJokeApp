e1bf29dd836fba45589343eeeb9e6817fdd7c7e3
Michael Kalisvaart <mkalisvaart@bol.com>
2018-01-26 14:09:11 +0100

    commeted xml

--- src/main/java/com/mkalisvaart/jokes/JokesApplication.java
+++ src/main/java/com/mkalisvaart/jokes/JokesApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@ImportResource("classpath:chuck-config.xml")
 public class JokesApplication {
 
 	public static void main(String[] args) {
